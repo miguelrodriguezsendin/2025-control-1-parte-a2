744af8fa297db1d74aebdc8b67693e81ded9bf50
miguel <miguelrodriguezsendin@gmail.com>
2025-11-06 17:31:43 +0100

    codigo implementado

--- src/main/java/es/grise/upm/profundizacion/subscriptionService/SubscriptionService.java
+++ src/main/java/es/grise/upm/profundizacion/subscriptionService/SubscriptionService.java
@@ -1,32 +1,45 @@
 package es.grise.upm.profundizacion.subscriptionService;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class SubscriptionService {
 
-	private Collection <User> subscribers;
-	
-	/*
-	 * Constructor
-	 */
-	public SubscriptionService(Delivery delivery) {
-		
-	}
+    private Collection<User> subscribers;
 
-	/*
-	 * Method to code
-	 */
-	public void addSubscriber(User user) {
-		
-	}
-	
-	/*
-	 * Other setters & getters
-	 */
-	public Collection <User> getSubscribers() {
-		
-		return subscribers;
-		
-	}
+    /*
+     * Constructor
+     */
+    public SubscriptionService() {
+        this.subscribers = new ArrayList<User>(); 
+    }
 
+    /*
+     * Method to code
+     */
+    public void addSubscriber(User user)
+            throws NullUserException, ExistingUserException, LocalUserDoesNotHaveNullEMailException {
+
+        if (user == null) {
+            throw new NullUserException("Usuario nulo");
+        }
+
+        if (subscribers.contains(user)) {
+            throw new ExistingUserException("Usuario existente");
+        }
+
+        if (user.getDelivery() == Delivery.LOCAL && user.getEmail() != null) {
+            throw new LocalUserDoesNotHaveNullEMailException("No tiene email");
+        }
+        subscribers.add(user);
+    }
+
+    /*
+     * Other setters & getters
+     */
+    public Collection<User> getSubscribers() {
+        return subscribers;
+    }
 }
+
+
