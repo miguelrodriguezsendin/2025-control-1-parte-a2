5877dd8c1bfce8d86ef05c29ec475e2f5850b310
miguel <miguelrodriguezsendin@gmail.com>
2025-11-06 17:48:22 +0100

    codigo implementado

--- src/test/java/es/grise/upm/profundizacion/subscriptionService/SubscriptionServiceTest.java
+++ src/test/java/es/grise/upm/profundizacion/subscriptionService/SubscriptionServiceTest.java
@@ -1,5 +1,49 @@
 package es.grise.upm.profundizacion.subscriptionService;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import java.util.Collection;
+
 public class SubscriptionServiceTest {
-	
+
+    private SubscriptionService service;
+
+    @BeforeEach
+    public void setUp() {
+        service = new SubscriptionService();
+    }
+
+    @Test
+    public void testInitialSubscribersListIsEmpty() {
+        Collection<User> subscribers = service.getSubscribers();
+        assertNotNull(subscribers);
+        assertTrue(subscribers.isEmpty());
+    }
+
+    @Test
+    public void testAddNullUser() {
+        assertThrows(NullUserException.class, () -> service.addSubscriber(null));
+    }
+
+    @Test
+    public void testAddExistingUser() throws Exception {
+        User user = Mockito.mock(User.class);
+        Mockito.when(user.getEmail()).thenReturn("test@ejemplo.com");
+        Mockito.when(user.getDelivery()).thenReturn(Delivery.DO_NOT_DELIVER);
+
+        service.addSubscriber(user);
+        assertThrows(ExistingUserException.class, () -> service.addSubscriber(user));
+    }
+
+    @Test
+    public void testLocalUserConEmail() {
+        User user = Mockito.mock(User.class);
+        Mockito.when(user.getEmail()).thenReturn("local@ejemplo.com");
+        Mockito.when(user.getDelivery()).thenReturn(Delivery.LOCAL);
+
+        assertThrows(LocalUserDoesNotHaveNullEMailException.class, () -> service.addSubscriber(user));
+    }
+
 }
